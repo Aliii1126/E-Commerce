4a0f8efa7bc1ab7b729a85fd24b4b7f8ea4606e7
Ali Nabilah Ramadhan <alinabilahramadhan@gmail.com>
2022-02-04 08:42:06 +0700

    Create Slider with Pure React

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+    id: 1,
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/f8b1fa80832453.5cecf8e50ae19.png",
+    title: "RED VELVET",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+    },
+    {
+    id: 2,
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/b2c52a80832453.5cecf8e50a77b.png",
+    title: "EXID",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+    },
+    {
+    id: 3,
+    img: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/a3dbd980832453.5cecf8e50a0e1.png",
+    title: "NATURE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+    },
+];
