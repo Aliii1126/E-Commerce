e1c652978ddf09dee3e962b42ecd141c405b609f
Ali Nabilah Ramadhan <alinabilahramadhan@gmail.com>
2022-07-14 13:45:44 +0700

    add products list

--- src/data.js
+++ src/data.js
@@ -39,3 +39,62 @@ export const categories = [
         title:"Shoes"
     },
 ];
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/451804ed-ac92-4849-8e42-5e8440d2fc41/sportswear-t-shirt-SVm4hj.png",
+    },
+    {
+        id:2,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/18526388-3986-49a8-b787-226c198cd8bf/solo-swoosh-1-4-zip-top-sv47Jt.png",
+    },
+    {
+        id:3,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/f756c1b4-7517-4f96-b710-23a98bccf943/hello-kitty-fleece-hoodie-hRRkb7.png",
+    },
+    {
+        id:4,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/992badac-4038-49bb-a39d-04c8d0651688/jordan-zion-long-sleeve-t-shirt-BRDnfX.png",
+    },
+    {
+        id:5,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/3097bec1-b2ac-48ec-a0fe-65b779847e41/naomi-osaka-collection-cropped-tennis-top-trPTd2.png",
+    },
+    {
+        id:6,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/0560c2fd-b524-4868-a683-817ee86fc9b0/sportswear-oversized-fleece-crew-top-xVp5rS.png",
+    },
+    {
+        id:7,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/ae5a133b-db2e-4afe-8774-0a07ceedc571/sportswear-t-shirt-lgW6b6.png",
+    },
+    {
+        id:8,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/fa799d44-da05-440d-9f09-b74c6615aadb/run-division-jacket-LthvgK.png",
+    },
+    {
+        id:9,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/c8faea0e-5de9-41d6-b2c3-556e10a31791/solo-swoosh-fleece-hoodie-dbBmL4.png",
+    },
+    {
+        id:10,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/148cb953-5b8c-4675-825d-08050188d684/sportswear-essential-woven-high-waisted-shorts-9rg0R7.png",
+    },
+    {
+        id:11,
+        img:"https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/seflixy5qzkejhfzyr4d/sportswear-essential-cropped-t-shirt-D8M7MX.png",
+    },
+    {
+        id:12,
+        img:"https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/9c55c1e3-de6a-45d4-ad8f-7f9e0810458c/dri-fit-dye-training-tank-dXPMsF.png",
+    },
+    {
+        id:13,
+        img:"https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/59410d69-2b5f-493a-805d-12fdfa9823e2/sportswear-icon-clash-short-sleeve-tie-top-GdbMh0.png",
+    },
+    {
+        id:14,
+        img:"https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/21858816-12d4-48ea-82dc-f0d808dcd3e1/sportswear-max90-t-shirt-tdVGGv.png",
+    }
+];
